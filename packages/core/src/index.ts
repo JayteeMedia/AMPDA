36194e1a633f2235df3e656c0981cea824acd521
Jay Sixx <jaysontavares@gmail.com>
2026-08-17 00:10:19 -0300

    0002-core-package

---
+++ packages/core/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * AMPDA Core
+ * Foundation package for the AMPDA platform.
+ */
+
+export const CORE_PACKAGE_NAME = "@ampda/core";
+export const CORE_VERSION = "0.1.0";
+
+export function getCoreInfo() {
+  return {
+    name: CORE_PACKAGE_NAME,
+    version: CORE_VERSION,
+  };
+}
